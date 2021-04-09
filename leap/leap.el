9591d706efe172c095e163fc247ee808e996aa48
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-09 21:04:01 +0200

    Solve exercise "Leap"

--- leap/leap.el
+++ leap/leap.el
@@ -4,6 +4,16 @@
 
 ;;; Code:
 
+(defun leap-year-p (y)
+  (and
+   (zerop (% y 4))
+   (or
+    (not (zerop (% y 100)))
+    (zerop (% y 400))
+    )
+   )
+  )
 
 (provide 'leap-year-p)
+
 ;;; leap.el ends here
